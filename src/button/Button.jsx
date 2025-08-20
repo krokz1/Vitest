24c7798f41877654fc8ca3083de8aebc170b42a8
Nikolay Rogatin <nikolaj.rogatin201@gmail.com>
2025-08-20 23:25:40 +0700

    настройка Vitest

---
+++ src/button/Button.jsx
@@ -0,0 +1,10 @@
+import './button.css'
+
+export default function Button({ children, onClick }) {
+
+    return (
+        <button className="button" onClick={onClick}>
+            {children}
+        </button>
+    )
+}
